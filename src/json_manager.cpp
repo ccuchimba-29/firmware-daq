8a11f914c36c57a1e38e4edaec592cebb9c9d77d
ccuchimba-29 <ccuchimba29@gmail.com>
2026-07-30 23:43:08 -0500

    Agregar nombre de dispositivo a archivo JSON

--- src/json_manager.cpp
+++ src/json_manager.cpp
@@ -5,11 +5,12 @@ JsonDocument json;
 void jsonCreateMessage()
 {
     json.clear();
-    json["type"] = "temperature";
+    json["device"] = "ESP32_N1";
 }
 
 void setJsonMessage(const float temps[12]){
     json.clear();
+    json["device"] = "ESP32_N1";
     json["type"] = "temperature";
     JsonArray tempArray = json["temperatures"].to<JsonArray>();
     for (uint8_t i = 0; i < 12; i++)
