d9cf4f09a1c1acdc3a0b4fbe6216d99c820f542f
ccuchimba-29 <ccuchimba29@gmail.com>
2026-07-30 20:11:31 -0500

    Agregar mDNS al esp32

--- src/wifi_manager.cpp
+++ src/wifi_manager.cpp
@@ -1,5 +1,6 @@
 #include <wifi_manager.h>
 #include <WiFi.h>
+#include <ESPmDNS.h>
 
 
 const char* ssid = "CCUCHIMBA 29";
@@ -7,15 +8,16 @@ const char* password = "set@06072001";
 
 void connectWiFi(){
     WiFi.begin(ssid, password);
-
     Serial.print("Conectando");
-
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
         Serial.print(".");
     }
 
+    if(MDNS.begin("esp32-daq")){
+        Serial.println("mDNS Iniciado");
+    }
     Serial.println();
     Serial.println("WiFi conectado");
     Serial.print("IP: ");
